352f5663980d2a45fec7b231250acd647d93907a
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2019-12-06 11:26:40 -0500

    Nav Animations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let nav = document.querySelectorAll("nav a")
+nav.forEach(link => {link.addEventListener("mouseenter", (event) => {link.style.fontSize = "2rem";})})
+nav.forEach(link => {link.addEventListener("mouseleave", (event) => {link.style.fontSize = "1.6rem";})})
+nav.forEach(link => {link.addEventListener("mouseenter", (event) => {link.style.borderBottom = "solid yellow 2px";})})
+nav.forEach(link => {link.addEventListener("mouseleave", (event) => {link.style.borderBottom = "none";})})
